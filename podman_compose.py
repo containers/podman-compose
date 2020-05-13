64ed5545437c1348b65b5f9a4298c2212d3d6419
Mohamed Akram <am@razrlab.com>
2020-05-13 14:28:33 +0400

    Allow empty default/error value in substitution

--- podman_compose.py
+++ podman_compose.py
@@ -146,8 +146,8 @@ def fix_mount_dict(mount_dict, proj_name, srv_name):
 # $$ means $
 
 var_re = re.compile(r'\$(\{(?:[^\s\$:\-\}]+)\}|(?:[^\s\$\{\}]+))')
-var_def_re = re.compile(r'\$\{([^\s\$:\-\}]+)(:)?-([^\}]+)\}')
-var_err_re = re.compile(r'\$\{([^\s\$:\-\}]+)(:)?\?([^\}]+)\}')
+var_def_re = re.compile(r'\$\{([^\s\$:\-\}]+)(:)?-([^\}]*)\}')
+var_err_re = re.compile(r'\$\{([^\s\$:\-\}]+)(:)?\?([^\}]*)\}')
 
 def dicts_get(dicts, key, fallback='', fallback_empty=False):
     """
