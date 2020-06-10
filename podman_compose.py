b2eaca00735463ffa03b02c4cbd88754e3c84c21
Waldemar Znoinski <wznoinsk@redhat.com>
2020-06-10 16:14:47 +0100

    fixes for podman v1.6.4

--- podman_compose.py
+++ podman_compose.py
@@ -374,7 +374,7 @@ def mount_dict_vol_to_bind(compose, mount_dict):
     except subprocess.CalledProcessError:
         compose.podman.output(["volume", "create", "-l", "io.podman.compose.project={}".format(proj_name), vol_name])
         out = compose.podman.output(["volume", "inspect", vol_name])
-    src = json.loads(out)[0]["mountPoint"]
+    src = json.loads(out)[0]["mountPoint"] or json.loads(out)[0]["Mountpoint"]
     ret=dict(mount_dict, type="bind", source=src, _vol=vol_name)
     bind_prop=ret.get("bind", {}).get("propagation")
     if not bind_prop:
