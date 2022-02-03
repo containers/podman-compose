33c31ae4a114d8b4b15988fd134289aa93f5d2e8
Cody Hutchins <codyhutchins@seed-innovations.com>
2022-02-03 13:37:03 -0700

    making changed file more production

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src._podman_compose import main
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from podman_compose.podman_compose import main
 
 
 if __name__ == "__main__":
--- podman_compose/classes.py
+++ podman_compose/classes.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import argparse
 import json
 import os
