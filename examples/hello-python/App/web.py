ee7029fbde1a1c8f1effaa6a8ea08725de6b8a93
Muayyad alsadi <alsadi@gmail.com>
2022-05-10 02:32:08 +0300

    reformat

--- examples/hello-python/App/web.py
+++ examples/hello-python/App/web.py
@@ -4,30 +4,34 @@ import asyncio
 import aioredis
 from aiohttp import web
 
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = int(os.environ.get('REDIS_PORT', '6379'))
-REDIS_DB = int(os.environ.get('REDIS_DB', '0'))
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.environ.get("REDIS_PORT", "6379"))
+REDIS_DB = int(os.environ.get("REDIS_DB", "0"))
 
 redis = aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}")
 app = web.Application()
 routes = web.RouteTableDef()
 
-@routes.get('/')
+
+@routes.get("/")
 async def hello(request):
     counter = await redis.incr("mycounter")
     return web.Response(text=f"counter={counter}")
 
-@routes.get('/hello.json')
+
+@routes.get("/hello.json")
 async def hello_json(request):
     counter = await redis.incr("mycounter")
-    data = {'counter': counter}
+    data = {"counter": counter}
     return web.json_response(data)
 
+
 app.add_routes(routes)
+
 
 def main():
     web.run_app(app)
 
-if __name__=='__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
