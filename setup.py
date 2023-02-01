1ffd24dcf92e4867671156a8d6bab42b0669618b
Cleber Rosa <crosa@redhat.com>
2023-02-01 14:21:11 -0500

    Python version support: sync verified and advertised versions
    There are differences with regards to the versions of Python that are
    verified (somehow) through Pylint, and the ones that are advertised.
    
    Given that there's no pinning of Pylint versions, it shouldn't be
    possible to use it on Python versions such as 3.5 and 3.6 (latest
    Pylint doesn't support those).  With that, let's cover all the
    currently supported Python versions.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- setup.py
+++ setup.py
@@ -16,12 +16,11 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
