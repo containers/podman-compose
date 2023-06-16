e8b9d3a9d7b6cfe4207fee0aca71358bf5589e55
Randolph Sapp <res.sapp@gmail.com>
2023-06-16 01:32:39 -0500

    podman_compose: fix in-pod argparse type
    If "bool" is provided as an argparse type it will only evaluate to false
    if an empty string is provided. This lambda attempts to resolve this by
    filtering for "1" or "true".
    
    Signed-off-by: Randolph Sapp <res.sapp@gmail.com>

--- podman_compose.py
+++ podman_compose.py
@@ -1799,7 +1799,7 @@ class PodmanCompose:
             "--in-pod",
             help="pod creation",
             metavar="in_pod",
-            type=bool,
+            type=lambda x: x.lower() in ["1", "true"],
             default=True,
         )
         parser.add_argument(
