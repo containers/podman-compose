b34f699adbbb4190c2afebcbe114cd6e1b37ea98
Povilas Kanapickas <povilas@radix.lt>
2024-03-08 12:19:12 +0200

    Add SPDX license identifiers to each source file
    Signed-off-by: Povilas Kanapickas <povilas@radix.lt>

--- tests/test_podman_compose.py
+++ tests/test_podman_compose.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0
+
 from pathlib import Path
 import subprocess
 import os
--- tests/test_podman_compose_config.py
+++ tests/test_podman_compose_config.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0
+
 """
 test_podman_compose_config.py
 
