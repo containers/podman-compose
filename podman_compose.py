980417d77e07c00342c876e416e4493f095d6531
Strahil Nikolov <stnikolov@liveperson.com>
2024-05-31 11:50:16 +0300

    Add --health-on-failure
    Signed-off-by: Strahil Nikolov <stnikolov@liveperson.com>

--- podman_compose.py
+++ podman_compose.py
@@ -1218,6 +1218,10 @@ async def container_to_args(compose, cnt, detached=True):
     if "start_period" in healthcheck:
         podman_args.extend(["--healthcheck-start-period", healthcheck["start_period"]])
 
+    # check if any action must be taken on healtcheck failure
+    if "on_failure" in healthcheck:
+        podman_args.extend(["--health-on-failure", healthcheck["on_failure"]])
+
     # convert other parameters to string
     if "retries" in healthcheck:
         podman_args.extend(["--healthcheck-retries", str(healthcheck["retries"])])
