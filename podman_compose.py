0f645e4c70878aa578b6234ad8919e917f20781c
Artho Pacini <eu@arthopacini.com>
2024-06-17 10:39:02 -0300

    Add ability to input docker-compose.yaml via stdin
    Signed-off-by: Artho Pacini <eu@arthopacini.com>

--- podman_compose.py
+++ podman_compose.py
@@ -2051,7 +2051,7 @@ class PodmanCompose:
         parser.add_argument(
             "-f",
             "--file",
-            help="Specify an alternate compose file (default: docker-compose.yml). If '-' is specified, the input will be read from standard input (stdin).",
+            help="Specify an alternate compose file (default: docker-compose.yml) or '-' for reading from stdin.",
             metavar="file",
             action="append",
             default=[],
