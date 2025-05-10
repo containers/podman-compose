abe5965c9a9d276cf2be5ed22fbc6d4a73461ba0
Justin Zhang <schnell18@gmail.com>
2025-05-10 14:01:43 +0300

    tests: Improve reliability of network tests
    The test did fail on my laptop with podman 5.4.1.
    
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- tests/integration/nets_test1/test_podman_compose_nets_test1.py
+++ tests/integration/nets_test1/test_podman_compose_nets_test1.py
@@ -59,9 +59,13 @@ class TestComposeNetsTest1(unittest.TestCase, RunSubprocessMixin):
             )
 
             # check if Host port is the same as provided by the service port
+            self.assertIsNotNone(container_info['NetworkSettings']["Ports"].get("8001/tcp", None))
+            self.assertGreater(len(container_info['NetworkSettings']["Ports"]["8001/tcp"]), 0)
+            self.assertIsNotNone(
+                container_info['NetworkSettings']["Ports"]["8001/tcp"][0].get("HostPort", None)
+            )
             self.assertEqual(
-                container_info['NetworkSettings']["Ports"],
-                {"8001/tcp": [{"HostIp": "", "HostPort": "8001"}]},
+                container_info['NetworkSettings']["Ports"]["8001/tcp"][0]["HostPort"], "8001"
             )
 
             self.assertEqual(container_info["Config"]["Hostname"], "web1")
@@ -77,9 +81,13 @@ class TestComposeNetsTest1(unittest.TestCase, RunSubprocessMixin):
                 list(container_info["NetworkSettings"]["Networks"].keys())[0], "nets_test1_default"
             )
 
+            self.assertIsNotNone(container_info['NetworkSettings']["Ports"].get("8001/tcp", None))
+            self.assertGreater(len(container_info['NetworkSettings']["Ports"]["8001/tcp"]), 0)
+            self.assertIsNotNone(
+                container_info['NetworkSettings']["Ports"]["8001/tcp"][0].get("HostPort", None)
+            )
             self.assertEqual(
-                container_info['NetworkSettings']["Ports"],
-                {"8001/tcp": [{"HostIp": "", "HostPort": "8002"}]},
+                container_info['NetworkSettings']["Ports"]["8001/tcp"][0]["HostPort"], "8002"
             )
 
             self.assertEqual(container_info["Config"]["Hostname"], "web2")
