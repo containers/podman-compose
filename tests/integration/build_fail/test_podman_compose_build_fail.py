0b8b483cb7f08aa0a1b7bc926f7a741e001bf409
Monika Kairaityte <monika@kibit.lt>
2025-05-26 22:10:05 +0300

    Print full dockerfile path instead of context on error
    Current error message does not provide the exact path where the
    Dockerfile is being searched.
    
    This commit distinguishes two types of errors when Dockerfile is not
    found in build context:
    - when the context does not have provided custom-named Dockerfile, the
    path in the error message is absolute: "OSError: Dockerfile not found in
    .../podman-compose/tests/integration/build_fail/context_no_file/custom-named-Dockerfile"
    - when the context does not have any Dockerfile, the path in the error
    message is: "OSError: Dockerfile not found in
    .../podman-compose/tests/integration/build_fail/context_no_file"
    Only this error message was used before the fix.
    
    Signed-off-by: Monika Kairaityte <monika@kibit.lt>
    Co-authored-by: Siteshwar Vashisht <svashisht@redhat.com>

--- tests/integration/build_fail/test_podman_compose_build_fail.py
+++ tests/integration/build_fail/test_podman_compose_build_fail.py
@@ -22,9 +22,48 @@ class TestComposeBuildFail(unittest.TestCase, RunSubprocessMixin):
                 "-f",
                 compose_yaml_path(),
                 "build",
+                "test",
             ],
             expected_returncode=127,
         )
         self.assertIn("RUN this_command_does_not_exist", str(output))
         self.assertIn("this_command_does_not_exist: not found", str(error))
         self.assertIn("while running runtime: exit status 127", str(error))
+
+    def test_dockerfile_does_not_exist(self):
+        out, error = self.run_subprocess_assert_returncode(
+            [
+                podman_compose_path(),
+                "-f",
+                compose_yaml_path(),
+                "build",
+                "test_no_dockerfile",
+            ],
+            expected_returncode=1,
+        )
+        error = error.decode('utf-8')
+        result = '\n'.join(error.splitlines()[-1:])
+
+        expected_path = os.path.join(os.path.dirname(__file__), "context_no_file")
+        expected = f'OSError: Dockerfile not found in {expected_path}'
+
+        self.assertEqual(expected, result)
+
+    def test_custom_dockerfile_does_not_exist(self):
+        out, error = self.run_subprocess_assert_returncode(
+            [
+                podman_compose_path(),
+                "-f",
+                compose_yaml_path(),
+                "build",
+                "test_no_custom_dockerfile",
+            ],
+            expected_returncode=1,
+        )
+        error = error.decode('utf-8')
+        result = '\n'.join(error.splitlines()[-1:])
+
+        expected_path = os.path.join(os.path.dirname(__file__), "context_no_file/Dockerfile-alt")
+        expected = f'OSError: Dockerfile not found in {expected_path}'
+
+        self.assertEqual(expected, result)
