containers/podman-compose

commit e03d675b9bcd92864fd1aedc23d92f72e410a54d

Remove incorrect os.chdir call to fix folder error
Signed-off-by: notdian <dian@fishekqi.com>

podman_compose.py
@@ -1925,9 +1925,6 @@ class PodmanCompose:
             dotenv_path = os.path.realpath(args.env_file)
             dotenv_dict.update(dotenv_to_dict(dotenv_path))
 
-        # TODO: remove next line
-        os.chdir(dirname)
-
         os.environ.update({
             key: value for key, value in dotenv_dict.items() if key.startswith("PODMAN_")
         })
@@ -1969,11 +1966,18 @@ class PodmanCompose:
             content = normalize(content)
             # log(filename, json.dumps(content, indent = 2))
             content = rec_subs(content, self.environ)
+            if isinstance(services := content.get('services'), dict):
+                for service in services.values():
+                    if 'extends' in service and (service_file := service['extends'].get('file')):
+                        service['extends']['file'] = os.path.join(
+                            os.path.dirname(filename), service_file
+                        )
+
             rec_merge(compose, content)
             # If `include` is used, append included files to files
             include = compose.get("include")
             if include:
-                files.extend(include)
+                files.extend([os.path.join(os.path.dirname(filename), i) for i in include])
                 # As compose obj is updated and tested with every loop, not deleting `include`
                 # from it, results in it being tested again and again, original values for
                 # `include` be appended to `files`, and, included files be processed for ever.

commit 0491269f5316cdef30df67922d918b1530b0dccf

Fix relative host path resolution for volume bind mount source
e03d675b9bcd92864fd1aedc23d92f72e410a54d broke relative host path
resolution by deleting os.chdir(). After this commit current working
directory is not relevant anymore.

Fixes e03d675b9bcd92864fd1aedc23d92f72e410a54d.

Signed-off-by: Monika Kairaityte <monika@kibit.lt>

podman_compose.py
@@ -421,6 +421,7 @@ async def assert_volume(compose: PodmanCompose, mount_dict: dict[str, Any]) -> N
                 os.makedirs(mount_src, exist_ok=True)
             except OSError:
                 pass
+        mount_dict["source"] = mount_src
         return
     if mount_dict["type"] != "volume" or not vol or not vol.get("name"):
         return

tests/integration/selinux/test_podman_compose_selinux.py
@@ -36,8 +36,9 @@ class TestPodmanCompose(unittest.TestCase, RunSubprocessMixin):
                 "selinux_container1_1",
             ])
             inspect_out = json.loads(out)
-            create_command_list = inspect_out[0].get("Config", []).get("CreateCommand", {})
-            self.assertIn('./host_test_text.txt:/test_text.txt:z', create_command_list)
+            create_command_list = inspect_out[0].get("Config", []).get("CreateCommand", [])
+            host_path = os.path.join(test_path(), "selinux", "host_test_text.txt")
+            self.assertIn(f'{host_path}:/test_text.txt:z', create_command_list)
 
             out, _ = self.run_subprocess_assert_returncode([
                 "podman",
@@ -45,8 +46,9 @@ class TestPodmanCompose(unittest.TestCase, RunSubprocessMixin):
                 "selinux_container2_1",
             ])
             inspect_out = json.loads(out)
-            create_command_list = inspect_out[0].get("Config", []).get("CreateCommand", {})
-            self.assertIn('./host_test_text.txt:/test_text.txt', create_command_list)
+            create_command_list = inspect_out[0].get("Config", []).get("CreateCommand", [])
+            host_path = os.path.join(test_path(), "selinux", "host_test_text.txt")
+            self.assertIn(f'{host_path}:/test_text.txt', create_command_list)
         finally:
             out, _ = self.run_subprocess_assert_returncode([
                 podman_compose_path(),

tests/unit/test_container_to_args.py
@@ -531,10 +531,24 @@ class TestContainerToArgs(unittest.IsolatedAsyncioTestCase):
         )
 
     @parameterized.expand([
-        (False, "z", ["--mount", "type=bind,source=./foo,destination=/mnt,z"]),
-        (False, "Z", ["--mount", "type=bind,source=./foo,destination=/mnt,Z"]),
-        (True, "z", ["-v", "./foo:/mnt:z"]),
-        (True, "Z", ["-v", "./foo:/mnt:Z"]),
+        (
+            False,
+            "z",
+            [
+                "--mount",
+                f"type=bind,source={get_test_file_path('test_dirname/foo')},destination=/mnt,z",
+            ],
+        ),
+        (
+            False,
+            "Z",
+            [
+                "--mount",
+                f"type=bind,source={get_test_file_path('test_dirname/foo')},destination=/mnt,Z",
+            ],
+        ),
+        (True, "z", ["-v", f"{get_test_file_path('test_dirname/foo')}:/mnt:z"]),
+        (True, "Z", ["-v", f"{get_test_file_path('test_dirname/foo')}:/mnt:Z"]),
     ])
     async def test_selinux_volume(
         self, prefer_volume: bool, selinux_type: str, expected_additional_args: list
@@ -556,6 +570,62 @@ class TestContainerToArgs(unittest.IsolatedAsyncioTestCase):
                 "bind": {
                     "selinux": selinux_type,
                 },
+            }
+        ]
+
+        args = await container_to_args(c, cnt)
+        self.assertEqual(
+            args,
+            [
+                "--name=project_name_service_name1",
+                "-d",
+                *expected_additional_args,
+                "--network=bridge:alias=service_name",
+                "busybox",
+            ],
+        )
+
+    @parameterized.expand([
+        (
+            "absolute_path",
+            get_test_file_path('test_dirname/foo'),
+            [
+                "--mount",
+                f"type=bind,source={get_test_file_path('test_dirname/foo')},destination=/mnt",
+            ],
+        ),
+        (
+            "relative_path",
+            './foo',
+            [
+                "--mount",
+                f"type=bind,source={get_test_file_path('test_dirname/foo')},destination=/mnt",
+            ],
+        ),
+        (
+            "home_dir",
+            '~/test_dirname/foo',
+            [
+                "--mount",
+                f"type=bind,source={os.path.expanduser('~/test_dirname/foo')},destination=/mnt",
+            ],
+        ),
+    ])
+    async def test_volumes_bind_mount_source(
+        self, test_name: str, mount_source: str, expected_additional_args: list
+    ) -> None:
+        c = create_compose_mock()
+        cnt = get_minimal_container()
+
+        # This is supposed to happen during `_parse_compose_file`
+        # but that is probably getting skipped during testing
+        cnt["_service"] = cnt["service_name"]
+
+        cnt["volumes"] = [
+            {
+                "type": "bind",
+                "source": f"{mount_source}",
+                "target": "/mnt",
             }
         ]
 