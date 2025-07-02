b3fd55047b9455639928f3ef216919638b115419
Monika Kairaityte <monika@kibit.lt>
2025-07-02 20:43:47 +0300

    tests/integration: Rename dir `build_fail_multi`
    Renamed directory `build_fail_multi` to more appropriate
    `commands_fail_exit_code` as more tests were added to other
    commands:`push` and `run`. Names of tests were changed accordingly.
    
    Signed-off-by: Monika Kairaityte <monika@kibit.lt>

--- tests/integration/commands_fail_exit_code/test_podman_compose_commands_fail_exit_code.py
+++ tests/integration/commands_fail_exit_code/test_podman_compose_commands_fail_exit_code.py
@@ -8,14 +8,14 @@ from tests.integration.test_utils import podman_compose_path
 from tests.integration.test_utils import test_path
 
 
-def compose_yaml_path():
+def compose_yaml_path() -> str:
     """ "Returns the path to the compose file used for this test module"""
-    base_path = os.path.join(test_path(), "build_fail_multi")
+    base_path = os.path.join(test_path(), "commands_fail_exit_code")
     return os.path.join(base_path, "docker-compose.yml")
 
 
-class TestComposeBuildFailMulti(unittest.TestCase, RunSubprocessMixin):
-    def test_build_fail_multi(self):
+class TestComposeCommandsFailExitCodes(unittest.TestCase, RunSubprocessMixin):
+    def test_build_command_fail(self) -> None:
         output, error = self.run_subprocess_assert_returncode(
             [
                 podman_compose_path(),
