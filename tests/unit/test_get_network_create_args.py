c7bce311db8dc9571c09914400b58b20def2437c
Povilas Kanapickas <povilas@radix.lt>
2025-07-06 19:48:16 +0300

    tests: Extract get_minimal_net_desc() in network tests
    Signed-off-by: Povilas Kanapickas <povilas@radix.lt>

--- tests/unit/test_get_network_create_args.py
+++ tests/unit/test_get_network_create_args.py
@@ -1,11 +1,12 @@
 import unittest
+from typing import Any
 
 from podman_compose import get_network_create_args
 
 
 class TestGetNetworkCreateArgs(unittest.TestCase):
-    def test_minimal(self) -> None:
-        net_desc = {
+    def get_minimal_net_desc(self) -> dict[str, Any]:
+        return {
             "labels": [],
             "internal": False,
             "driver": None,
@@ -13,6 +14,9 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
             "ipam": {"config": []},
             "enable_ipv6": False,
         }
+
+    def test_minimal(self) -> None:
+        net_desc = self.get_minimal_net_desc()
         proj_name = "test_project"
         net_name = "test_network"
         expected_args = [
@@ -27,14 +31,8 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
     def test_ipv6(self) -> None:
-        net_desc = {
-            "labels": [],
-            "internal": False,
-            "driver": None,
-            "driver_opts": {},
-            "ipam": {"config": []},
-            "enable_ipv6": True,
-        }
+        net_desc = self.get_minimal_net_desc()
+        net_desc["enable_ipv6"] = True
         proj_name = "test_project"
         net_name = "test_network"
         expected_args = [
@@ -50,14 +48,9 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
     def test_bridge(self) -> None:
-        net_desc = {
-            "labels": [],
-            "internal": False,
-            "driver": "bridge",
-            "driver_opts": {"opt1": "value1", "opt2": "value2"},
-            "ipam": {"config": []},
-            "enable_ipv6": False,
-        }
+        net_desc = self.get_minimal_net_desc()
+        net_desc["driver"] = "bridge"
+        net_desc["driver_opts"] = {"opt1": "value1", "opt2": "value2"}
         proj_name = "test_project"
         net_name = "test_network"
         expected_args = [
@@ -78,21 +71,16 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
     def test_ipam_driver_default(self) -> None:
-        net_desc = {
-            "labels": [],
-            "internal": False,
-            "driver": None,
-            "driver_opts": {},
-            "ipam": {
-                "driver": "default",
-                "config": [
-                    {
-                        "subnet": "192.168.0.0/24",
-                        "ip_range": "192.168.0.2/24",
-                        "gateway": "192.168.0.1",
-                    }
-                ],
-            },
+        net_desc = self.get_minimal_net_desc()
+        net_desc["ipam"] = {
+            "driver": "default",
+            "config": [
+                {
+                    "subnet": "192.168.0.0/24",
+                    "ip_range": "192.168.0.2/24",
+                    "gateway": "192.168.0.1",
+                }
+            ],
         }
         proj_name = "test_project"
         net_name = "test_network"
@@ -114,21 +102,16 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
     def test_ipam_driver(self) -> None:
-        net_desc = {
-            "labels": [],
-            "internal": False,
-            "driver": None,
-            "driver_opts": {},
-            "ipam": {
-                "driver": "someipamdriver",
-                "config": [
-                    {
-                        "subnet": "192.168.0.0/24",
-                        "ip_range": "192.168.0.2/24",
-                        "gateway": "192.168.0.1",
-                    }
-                ],
-            },
+        net_desc = self.get_minimal_net_desc()
+        net_desc["ipam"] = {
+            "driver": "someipamdriver",
+            "config": [
+                {
+                    "subnet": "192.168.0.0/24",
+                    "ip_range": "192.168.0.2/24",
+                    "gateway": "192.168.0.1",
+                }
+            ],
         }
         proj_name = "test_project"
         net_name = "test_network"
@@ -203,15 +186,8 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
     def test_disable_dns(self) -> None:
-        net_desc = {
-            "labels": [],
-            "internal": False,
-            "driver": None,
-            "driver_opts": {},
-            "ipam": {"config": []},
-            "enable_ipv6": False,
-            "x-podman.disable_dns": True,
-        }
+        net_desc = self.get_minimal_net_desc()
+        net_desc["x-podman.disable_dns"] = True
         proj_name = "test_project"
         net_name = "test_network"
         expected_args = [
@@ -227,15 +203,8 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
     def test_dns_string(self) -> None:
-        net_desc = {
-            "labels": [],
-            "internal": False,
-            "driver": None,
-            "driver_opts": {},
-            "ipam": {"config": []},
-            "enable_ipv6": False,
-            "x-podman.dns": "192.168.1.2",
-        }
+        net_desc = self.get_minimal_net_desc()
+        net_desc["x-podman.dns"] = "192.168.1.2"
         proj_name = "test_project"
         net_name = "test_network"
         expected_args = [
@@ -252,15 +221,8 @@ class TestGetNetworkCreateArgs(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
     def test_dns_list(self) -> None:
-        net_desc = {
-            "labels": [],
-            "internal": False,
-            "driver": None,
-            "driver_opts": {},
-            "ipam": {"config": []},
-            "enable_ipv6": False,
-            "x-podman.dns": ["192.168.1.2", "192.168.1.3"],
-        }
+        net_desc = self.get_minimal_net_desc()
+        net_desc["x-podman.dns"] = ["192.168.1.2", "192.168.1.3"]
         proj_name = "test_project"
         net_name = "test_network"
         expected_args = [
