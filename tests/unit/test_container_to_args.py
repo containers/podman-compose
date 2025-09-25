f47a1e697cabf6ee78b72b36d95e433e809dca1a
Rafael Guterres Jeffman <rjeffman@redhat.com>
2025-09-25 18:13:34 -0300

    Support podman specific 'glob' mount type
    Add support for podman specific mount type 'glob', which allows a set of
    files to be made available under a mount point in the container.
    
    Signed-off-by: Rafael Guterres Jeffman <rjeffman@redhat.com>

--- tests/unit/test_container_to_args.py
+++ tests/unit/test_container_to_args.py
@@ -624,6 +624,41 @@ class TestContainerToArgs(unittest.IsolatedAsyncioTestCase):
             ],
         )
 
+    async def test_volumes_glob_mount_source(self) -> None:
+        c = create_compose_mock()
+        cnt = get_minimal_container()
+
+        # This is supposed to happen during `_parse_compose_file`
+        # but that is probably getting skipped during testing
+        cnt["_service"] = cnt["service_name"]
+
+        cnt["volumes"] = [
+            {
+                "type": "glob",
+                "source": f"{get_test_file_path('test_dirname/foo')}/*.ext",
+                "target": "/mnt",
+            }
+        ]
+        expected_additional_args = [
+            "--mount",
+            (
+                "type=glob,source="
+                f"{get_test_file_path('test_dirname/foo') + '/*.ext'},"
+                "destination=/mnt"
+            ),
+        ]
+        args = await container_to_args(c, cnt)
+        self.assertEqual(
+            args,
+            [
+                "--name=project_name_service_name1",
+                "-d",
+                *expected_additional_args,
+                "--network=bridge:alias=service_name",
+                "busybox",
+            ],
+        )
+
     @parameterized.expand([
         (
             "absolute_path",
