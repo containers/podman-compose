2ffac6619cf3e48cd1f22c26d13674ae5a8ecac6
Anton Petrov <antonpetrov145@yahoo.com>
2025-11-30 12:38:59 +0200

    tests/integration: Add test for test_podman_compose_include_dict()
    Signed-off-by: Anton Petrov <antonpetrov145@yahoo.com>
    Co-authored-by: Monika Kairaityte <monika@kibit.lt>

--- tests/integration/include/test_podman_compose_include.py
+++ tests/integration/include/test_podman_compose_include.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 
+import os
 import unittest
 from pathlib import Path
 
@@ -7,6 +8,12 @@ from packaging import version
 
 from tests.integration.test_utils import RunSubprocessMixin
 from tests.integration.test_utils import get_podman_version
+from tests.integration.test_utils import podman_compose_path
+from tests.integration.test_utils import test_path
+
+
+def compose_yaml_path(suffix: str = "") -> str:
+    return os.path.join(os.path.join(test_path(), "include"), f"docker-compose{suffix}.yaml")
 
 
 class TestPodmanComposeInclude(unittest.TestCase, RunSubprocessMixin):
@@ -66,3 +73,36 @@ class TestPodmanComposeInclude(unittest.TestCase, RunSubprocessMixin):
         # check container did not exist anymore
         out, _ = self.run_subprocess_assert_returncode(command_check_container)
         self.assertEqual(out, b"")
+
+    def test_podman_compose_include_dict(self) -> None:
+        try:
+            self.run_subprocess_assert_returncode([
+                "coverage",
+                "run",
+                podman_compose_path(),
+                "-f",
+                compose_yaml_path("_include_dict"),
+                "up",
+                "-d",
+            ])
+
+            out, _ = self.run_subprocess_assert_returncode([
+                "podman",
+                "ps",
+                "-a",
+                "--filter",
+                "label=io.podman.compose.project=include",
+                "--format",
+                '"{{.Names}}"',
+            ])
+            # two services from included compose files were created
+            self.assertEqual(out, b'"include_web_1"\n"include_web2_1"\n')
+        finally:
+            self.run_subprocess_assert_returncode([
+                podman_compose_path(),
+                "-f",
+                compose_yaml_path("_include_dict"),
+                "down",
+                "-t",
+                "0",
+            ])
