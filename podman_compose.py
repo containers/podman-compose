fa8e301135c837ce2784d57c0a8709d53d73977d
mammo0 <marc.ammon@hotmail.de>
2025-12-13 14:00:15 +0100

    fixed yaml dumper class of override and reset tag
    Signed-off-by: mammo0 <marc.ammon@hotmail.de>

--- podman_compose.py
+++ podman_compose.py
@@ -1500,7 +1500,7 @@ def flat_deps(services: dict[str, Any], with_extends: bool = False) -> None:
 
 
 class OverrideTag(yaml.YAMLObject):
-    yaml_dumper = yaml.Dumper
+    yaml_dumper = yaml.SafeDumper
     yaml_loader = yaml.SafeLoader
     yaml_tag = '!override'
 
@@ -1529,7 +1529,7 @@ class OverrideTag(yaml.YAMLObject):
 
 
 class ResetTag(yaml.YAMLObject):
-    yaml_dumper = yaml.Dumper
+    yaml_dumper = yaml.SafeDumper
     yaml_loader = yaml.SafeLoader
     yaml_tag = '!reset'
 
