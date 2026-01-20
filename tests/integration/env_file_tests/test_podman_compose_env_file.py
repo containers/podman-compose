0692ee02d960b1379710934707ac1616493e2a71
GerkinDev <alexandre.j.p.germain@gmail.com>
2026-01-20 10:05:40 +0100

    tests: actually check for precedence
    Signed-off-by: GerkinDev <alexandre.j.p.germain@gmail.com>

--- tests/integration/env_file_tests/test_podman_compose_env_file.py
+++ tests/integration/env_file_tests/test_podman_compose_env_file.py
@@ -66,7 +66,7 @@ class TestComposeEnvFile(unittest.TestCase, RunSubprocessMixin):
                 "logs",
             ])
             # takes only value ZZVAR1 as container-compose.yaml file requires
-            self.assertEqual(output, b"ZZVAR1=podman-rocks-123\nZZVAR4=podman-rocks-225\n")
+            self.assertEqual(output, b"ZZVAR1=podman-rocks-223\nZZVAR4=podman-rocks-225\n")
         finally:
             self.run_subprocess_assert_returncode([
                 podman_compose_path(),
