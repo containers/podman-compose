4ad5ac2a7ef4a127975f7d874c508c7dc6efc9c0
Yun Feng Ma <yma@ddn.com>
2026-02-24 03:29:12 +0000

    update integration test to check the cgroup memory configs in containr
    Signed-off-by: Yun Feng Ma <yma@ddn.com>

--- tests/integration/cgroup_conf/test_podman_compose_cgroup_conf.py
+++ tests/integration/cgroup_conf/test_podman_compose_cgroup_conf.py
@@ -31,6 +31,30 @@ class TestPodmanCompose(unittest.TestCase, RunSubprocessMixin):
             host_config_map = inspect_out[0].get("HostConfig", {}).get("CgroupConf", {})
             self.assertEqual('712M', host_config_map['memory.high'])
             self.assertEqual('800M', host_config_map['memory.max'])
+
+            # check memory.high in container
+            out, _ = self.run_subprocess_assert_returncode([
+                "podman",
+                "exec",
+                "-ti",
+                "cgroup_conf_cgroupconf_1",
+                "sh",
+                "-c",
+                "cat /sys/fs/cgroup/memory.high",
+            ])
+            self.assertEqual(out, b"746586112\r\n")
+
+            # check memory.max in container
+            out, _ = self.run_subprocess_assert_returncode([
+                "podman",
+                "exec",
+                "-ti",
+                "cgroup_conf_cgroupconf_1",
+                "sh",
+                "-c",
+                "cat /sys/fs/cgroup/memory.max",
+            ])
+            self.assertEqual(out, b"838860800\r\n")
         finally:
             out, _ = self.run_subprocess_assert_returncode([
                 podman_compose_path(),
