70d5af38012aacaee03afebed8dc7aeadb19c259
Erik van Oosten <e.vanoosten@grons.nl>
2026-03-15 13:17:14 +0100

    Increase compatibility for terminal/no-terminal options
    Increase compatibility with docker compose for exec and run commands:
    
    - Add support for long option `--no-tty` besides `-T`.
    - Add support for hidden options `-t` and `--tty`.
    - Make default for tty auto-detected.
    - For the `exec` command: run `podman exec` without `--interactive` when no-tty is specified (the `run` command already does this).
    - For `run`, fix adding --rm option when detached.
    
    The default for terminal/no-terminal is based on terminal detection, mimicking docker compose code for
    [run](https://github.com/docker/compose/blob/3371227794f5f3645f4f19829c60a741635ed329/cmd/compose/run.go#L178)
    and [exec](https://github.com/docker/compose/blob/3371227794f5f3645f4f19829c60a741635ed329/cmd/compose/exec.go#L72).
    
    Hidden options `-t` and `--tty`
    are mimicking docker compose code for [run](https://github.com/docker/compose/blob/3371227794f5f3645f4f19829c60a741635ed329/cmd/compose/run.go#L195-L196) and [exec](https://github.com/docker/compose/blob/3371227794f5f3645f4f19829c60a741635ed329/cmd/compose/exec.go#L77-L78).
    
    NOTE: some of the added integration tests require the `script` command to emulate a TTY. This command is available in most unix environments, but might not be present in a minimal docker images. When `script` is not available, these tests pass without doing anything.
    
    Fixes #900.
    
    Signed-off-by: Erik van Oosten <e.vanoosten@grons.nl>

---
+++ tests/integration/exec_tty/test_podman_compose_exec.py
@@ -0,0 +1,108 @@
+# SPDX-License-Identifier: GPL-2.0
+
+import os
+import subprocess
+import unittest
+
+from tests.integration.test_utils import RunSubprocessMixin
+from tests.integration.test_utils import podman_compose_path
+from tests.integration.test_utils import test_path
+
+
+def compose_yaml_path() -> str:
+    """Returns the path to the compose file used for this test module"""
+    base_path = os.path.join(test_path(), "exec_tty")
+    return os.path.join(base_path, "docker-compose.yml")
+
+
+class TestComposeExtraHosts(unittest.TestCase, RunSubprocessMixin):
+    def test_exec(self) -> None:
+        script_installed = (
+            subprocess.run(["script", "--version"], stdout=subprocess.DEVNULL).returncode == 0
+        )
+
+        try:
+            self.run_subprocess_assert_returncode([
+                podman_compose_path(),
+                "-f",
+                compose_yaml_path(),
+                "build",
+                "--no-cache",
+            ])
+
+            self.run_subprocess_assert_returncode([
+                podman_compose_path(),
+                "-f",
+                compose_yaml_path(),
+                "up",
+                "-d",
+            ])
+
+            # TTY auto detected (no tty)
+            self.run_subprocess_assert_returncode(
+                [
+                    podman_compose_path(),
+                    "-f",
+                    compose_yaml_path(),
+                    "exec",
+                    "web1",
+                    "tty",
+                    "-s",
+                ],
+                expected_returncode=1,  # exit code 1 == no tty
+            )
+
+            # TTY auto detected (tty emulated by 'script' command)
+            if script_installed:
+                self.run_subprocess_assert_returncode(
+                    [
+                        "script",
+                        "--return",
+                        "--quiet",
+                        "--log-out",
+                        "/dev/null",
+                        "--command",
+                        f"{podman_compose_path()} -f {compose_yaml_path()} exec web1 tty -s",
+                    ],
+                    expected_returncode=0,  # exit code 0 == tty
+                )
+
+            # TTY disabled (even though an emulated tty is available through the 'script' command)
+            if script_installed:
+                self.run_subprocess_assert_returncode(
+                    [
+                        "script",
+                        "--return",
+                        "--log-out",
+                        "/dev/null",
+                        "--quiet",
+                        "--command",
+                        f"{podman_compose_path()} "
+                        f"-f {compose_yaml_path()} "
+                        "exec --no-tty web1 tty -s",
+                    ],
+                    expected_returncode=1,  # exit code 1 == no tty
+                )
+
+            # TTY enabled (tty emulated by 'script' command)
+            if script_installed:
+                self.run_subprocess_assert_returncode(
+                    [
+                        "script",
+                        "--return",
+                        "--log-out",
+                        "/dev/null",
+                        "--quiet",
+                        "--command",
+                        f"{podman_compose_path()} -f {compose_yaml_path()} exec --tty web1 tty -s",
+                    ],
+                    expected_returncode=0,  # exit code 0 == tty
+                )
+
+        finally:
+            self.run_subprocess_assert_returncode([
+                podman_compose_path(),
+                "-f",
+                compose_yaml_path(),
+                "down",
+            ])
