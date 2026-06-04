b8273520f619399c7d3808ce6817653daacbd46f
wiehe <28457227+wiehe@users.noreply.github.com>
2026-06-04 17:42:55 +0200

    fixup! Add support for ipc option in services element
    Rewrite the remaining integration test.
    
    - Fetch information from containers with `podman-compose exec readlink`.
    - Make better use of functions from tests/integration/test_utils.py
    
    Signed-off-by: wiehe <28457227+wiehe@users.noreply.github.com>

--- tests/integration/ipc/test_podman_compose_ipc.py
+++ tests/integration/ipc/test_podman_compose_ipc.py
@@ -2,13 +2,10 @@
 
 """Test passing of ipc mode to podman."""
 
-from __future__ import annotations
-
 import os
-import re
-import subprocess
 import unittest
 
+from tests.integration.test_utils import RunSubprocessMixin
 from tests.integration.test_utils import podman_compose_path
 from tests.integration.test_utils import test_path
 
@@ -18,40 +15,46 @@ def compose_file_path(filename: str) -> str:
     return os.path.join(test_path(), "ipc", filename)
 
 
-def podman_compose_invoke(
-    compose_command: str, compose_file: str, service: str | None = None, compose_options: tuple = ()
-) -> subprocess.CompletedProcess:
-    """Helper function, execute a run of podman compose <command>"""
-    cmd = (
-        ("coverage", "run", podman_compose_path())
-        + compose_options
-        + ("-f", compose_file_path(compose_file), compose_command)
-    )
-
-    if service:
-        cmd += (service,)
-
-    return subprocess.run(
-        cmd,
-        stdout=subprocess.PIPE,
-        check=False,
-        stderr=subprocess.STDOUT,
-        text=True,
-    )
-
-
-class TestComposeIpc(unittest.TestCase):
-    def test_up_shared_namespace(self) -> None:
+class TestComposeIpc(unittest.TestCase, RunSubprocessMixin):
+    def test_ipc_shared_namespace(self) -> None:
         """Create and start two containers with shared ipc namespace"""
 
-        compose_file = "docker-compose-service.yaml"
+        compose_file = compose_file_path("docker-compose-service.yaml")
 
-        p = podman_compose_invoke("up", compose_file)
-        podman_compose_invoke("down", compose_file)
+        try:
+            # spin up yaml with two services
+            self.run_subprocess_assert_returncode([
+                podman_compose_path(),
+                "-f",
+                compose_file,
+                "up",
+                "-d",
+            ])
 
-        # check that both /proc/self/ns/ipc point to the same target
-        matches = re.findall(r'/proc/\d+/ns/ipc:.+', p.stdout)
-        self.assertEqual(len(matches), 2)
-        self.assertEqual(matches[0], matches[1])
-        self.assertNotIn("Error", p.stdout)
-        self.assertEqual(p.returncode, 0)
+            # read ipc namespace from both
+            ipc_namespaces = []
+
+            for service in ("ipc_test", "ipc_test0"):
+                output, _ = self.run_subprocess_assert_returncode([
+                    podman_compose_path(),
+                    "-f",
+                    compose_file,
+                    "exec",
+                    service,
+                    "readlink",
+                    "/proc/self/ns/ipc",
+                ])
+                ipc_namespaces.append(output)
+
+            # and check that they are equal
+            self.assertEqual(ipc_namespaces[0], ipc_namespaces[1])
+
+        finally:
+            self.run_subprocess_assert_returncode([
+                podman_compose_path(),
+                "-f",
+                compose_file,
+                "down",
+                "-t",
+                "0",
+            ])
