58b9f3d5b4567d708b425f57bd984e4a505e2d52
Monika Kairaityte <monika@kibit.lt>
2026-06-12 18:06:56 +0300

    tests/integration: Use parametrized in `include` test
    Signed-off-by: Monika Kairaityte <monika@kibit.lt>

--- tests/integration/include/test_podman_compose_include.py
+++ tests/integration/include/test_podman_compose_include.py
@@ -3,6 +3,8 @@
 import os
 import unittest
 
+from parameterized import parameterized
+
 from tests.integration.test_utils import RunSubprocessMixin
 from tests.integration.test_utils import podman_compose_path
 from tests.integration.test_utils import test_path
@@ -13,14 +15,18 @@ def compose_yaml_path(suffix: str = "") -> str:
 
 
 class TestPodmanComposeInclude(unittest.TestCase, RunSubprocessMixin):
-    def test_podman_compose_include_string(self) -> None:
+    @parameterized.expand([
+        ("string", ""),
+        ("dict", "_include_dict"),
+    ])
+    def test_podman_compose_include(self, name: str, compose_suffix: str) -> None:
         try:
             self.run_subprocess_assert_returncode([
                 "coverage",
                 "run",
                 podman_compose_path(),
                 "-f",
-                compose_yaml_path(),
+                compose_yaml_path(f"{compose_suffix}"),
                 "up",
                 "-d",
             ])
@@ -41,39 +47,6 @@ class TestPodmanComposeInclude(unittest.TestCase, RunSubprocessMixin):
                 podman_compose_path(),
                 "-f",
                 compose_yaml_path(),
-                "down",
-                "-t",
-                "0",
-            ])
-
-    def test_podman_compose_include_dict(self) -> None:
-        try:
-            self.run_subprocess_assert_returncode([
-                "coverage",
-                "run",
-                podman_compose_path(),
-                "-f",
-                compose_yaml_path("_include_dict"),
-                "up",
-                "-d",
-            ])
-
-            out, _ = self.run_subprocess_assert_returncode([
-                "podman",
-                "ps",
-                "-a",
-                "--filter",
-                "label=io.podman.compose.project=include",
-                "--format",
-                '"{{.Names}}"',
-            ])
-            # two services from included compose files were created
-            self.assertEqual(out, b'"include_web_1"\n"include_web2_1"\n')
-        finally:
-            self.run_subprocess_assert_returncode([
-                podman_compose_path(),
-                "-f",
-                compose_yaml_path("_include_dict"),
                 "down",
                 "-t",
                 "0",
