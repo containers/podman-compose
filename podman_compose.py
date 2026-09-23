6808c8ef255d13c01fc82d38c05694afad7e75cb
Monika Kairaityte <monika@kibit.lt>
2026-09-23 12:08:24 +0300

    Stop remaining containers on `--abort-on-container-exit`
    Before this change, when using `--abort-on-container-exit` or
    `--exit-code-from`, exiting one container would leave the remaining
    containers running in the background. Now, remaining containers are
    properly stopped when one container exits.
    
    Relevant docker-compose documentation:
    https://docs.docker.com/reference/cli/docker/compose/up/
    
    Signed-off-by: Monika Kairaityte <monika@kibit.lt>

--- podman_compose.py
+++ podman_compose.py
@@ -1946,6 +1946,7 @@ class Podman:
         *,
         suppress_output: bool = False,
         pass_stdin: bool = False,
+        container_name: str | None = None,
         # Intentionally mutable default argument to hold references to tasks
         task_reference: set[asyncio.Task] = set(),
     ) -> int | None:
@@ -2006,6 +2007,12 @@ class Podman:
             except asyncio.CancelledError:
                 log.info("Sending termination signal")
                 p.terminate()
+                if container_name:
+                    log.info("Stopping container %s", container_name)
+                    try:
+                        await self.run([], "stop", ["-t", "10", container_name])
+                    except Exception as e:  # pylint: disable=broad-exception-caught
+                        log.warning("Error stopping container %s: %s", container_name, e)
                 try:
                     exit_code = await wait_with_timeout(p.wait(), 10)
                 except TimeoutError:
@@ -3949,7 +3956,7 @@ async def run_container(
     # start the container
     log.debug("Starting task for container %s", name)
     return await compose.podman.run(  # type: ignore[misc]
-        *command, log_formatter=log_formatter, suppress_output=suppress_output
+        *command, log_formatter=log_formatter, suppress_output=suppress_output, container_name=name
     )
 
 
